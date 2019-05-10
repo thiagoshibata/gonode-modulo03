76f27d3d024e8b20392a507433749d7ed6ba8470
thiago shibata <thiago.shibatta@gmail.com>
2019-05-10 14:35:12 -0300

    comentários nas rotas

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,22 @@ const authMiddleware = require('./app/middlewares/auth')
 
 const controllers = require('./app/controllers')
 
+/* USER */
 routes.post('/users', controllers.UserController.store)
+/* END USER */
+
+/* SESSION */
 routes.post('/sessions', controllers.SessionController.store)
+/* END SESSION */
 
 routes.use(authMiddleware)
-/*  Ads */
+
+/* CRUD Ads */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
 routes.post('/ads', controllers.AdController.store)
 routes.put('/ads/:id', controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
+/* END CRUD Ads */
 
 module.exports = routes
