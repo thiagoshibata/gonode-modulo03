2d071f4a3ed4896b9c959c6e8e9d6277c67c5728
Thiago Shibata <thiago.shibatta@gmail.com>
2019-05-13 22:42:32 -0300

    1.0.3 -validação de campos

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
 const express = require('express')
-
+const validate = require('express-validation')
 const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 /* USER */
-routes.post('/users', controllers.UserController.store)
+routes.post('/users', validate(validators.User), controllers.UserController.store)
 /* END USER */
 
 /* SESSION */
-routes.post('/sessions', controllers.SessionController.store)
+routes.post('/sessions', validate(validators.Session), controllers.SessionController.store)
 /* END SESSION */
 
 routes.use(authMiddleware)
@@ -19,12 +20,12 @@ routes.use(authMiddleware)
 /* CRUD Ads */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 /* END CRUD Ads */
 
 /* PURCHASE  */
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post('/purchase', validate(validators.Purchase), controllers.PurchaseController.store)
 
 module.exports = routes
